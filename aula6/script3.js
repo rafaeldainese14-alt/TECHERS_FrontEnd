fddfcd53a903e3ac46944907163ae13c28bf13e4
rafaeldainese14-alt <rafaeldainese14@gmail.com>
2026-03-12 15:50:12 -0300

    feat: aula 6 add

---
+++ aula6/script3.js
@@ -0,0 +1,42 @@
+// console.log("Hello World" )
+
+// let idade = 12;
+// const nome = "Rafael"
+
+// console.log(nome)
+
+// console.log("Bem vindo,",nome)
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// for(let i = 10; i <= 0; i--){
+//     console.log(i)
+// }
+// let frutas = ["banana","kiwi","morango"]
+
+// console.log(frutas[0])
+
+// frutas[0] = "melao"
+
+// console.log(frutas[0])
+
+// for (let i = 0; i < frutas.length; i++){
+//     console.log(frutas[i])
+// }
+
+function mudarTudo(){
+    const titulo = document.getElementById("titulo")
+    titulo.innerText = "Lista de compras!"
+    titulo.style.color = "blue"
+
+    let itens = ["banana","kiwi","morango"]
+    let container = document.getElementById("lista-frutas")
+ 
+    container.innerHTML = ""
+    for(let i = 0; itens.length; i++){
+        container.innerHTML += `<p>Item $(i+1) $(itens[i]) </p>`
+    }
+
+}
